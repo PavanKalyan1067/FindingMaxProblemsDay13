3163eecd5105e2729b4c0213a39849ed5b32f81b
Pavan Kalyan <nikhilgoud@nikhils-MacBook-Air.local>
2022-04-02 12:25:00 +0530

    Finding Max Integer

--- Program.cs
+++ Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 
-namespace FindMaxDay13
+namespace FindMaxValue
 {
-    class Program
+    public class Program
     {
+        public int FindMaximum(int first, int second, int third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                return first;
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+            else if (third.CompareTo(second) > 0 && third.CompareTo(first) > 0)
+            {
+                return third;
+            }
+            else
+            {
+                Console.WriteLine("All three having same number");
+                return -1;
+            }
+        }
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program obj = new Program();
+            int maxValue = obj.FindMaximum(70, 35, 56);
+            Console.WriteLine(maxValue);
+            Console.ReadLine();
         }
     }
+
 }
